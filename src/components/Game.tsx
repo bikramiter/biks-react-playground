9d253772371f085a396638da7733b278a1581a06
Bikram Sahu <bikramiter@gmail.com>
2026-08-25 18:39:55 +0530

    feat: implement round one gameplay

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,21 +4,64 @@ import Card from "./Card";
 import { useState } from "react";
 
 function Game() {
-  const [selectedCardId, setSelectedCardId] = useState<number | null>(null);
+  const [feedback, setFeedback] = useState("");
+  const [lives, setLives] = useState(3);
+  const [foundCards, setFoundCards] = useState<number[]>([]);
+
+  const correctCards = cards.filter((card) => card.color === "blue");
+  // check if all correct cards found
+  const allCardsIdentified = correctCards.every((card) =>
+    foundCards.includes(card.id),
+  );
+
+  function correctCardSelected(cardId: number) {
+    //set feedback
+    setFeedback("correct");
+
+    //handle game logic
+    if (!foundCards.includes(cardId)) {
+      setFoundCards((previousFoundCards) => [...previousFoundCards, cardId]);
+    }
+  }
+
+  function wrongCardSelected() {
+    //set feedback
+    setFeedback("wrong");
+
+    //decrease lives
+    setLives((previousLives) => previousLives - 1);
+  }
 
   function handleCardClick(cardId: number) {
-    //alreary selected card clicked - unselect the card
-    //no card selected - select clicked card
-    //a different card clicked - select this one and unselect the previous one
-    cardId === selectedCardId
-      ? setSelectedCardId(null)
-      : setSelectedCardId(cardId);
+    if (allCardsIdentified) return;
+    // check if selected card is correct
+    const cardIsCorrect = correctCards.some((card) => card.id === cardId);
+    // if correct, keep selected and store in foundCards
+    if (cardIsCorrect) {
+      correctCardSelected(cardId);
+    }
+    // if incorrect, blink twice and revert to original state
+    // decrease life by 1
+    else wrongCardSelected();
   }
+
   return (
     <main className="game">
       <header className="game-header">
         <h1>Rule Breaker</h1>
         <p>Discover the hidden rule</p>
+        <div className="lives">
+          {Array.from({ length: lives }).map((_, index) => (
+            <span key={index}>❤️</span>
+          ))}
+        </div>
+        <div className="feedback">
+          {feedback === "correct" && "✅ Correct!"}
+          {feedback === "wrong" && "❌ Wrong!"}
+        </div>
+        {allCardsIdentified && (
+          <div className="round-complete">🎉 Round Complete!</div>
+        )}
       </header>
 
       <section className="board">
@@ -26,7 +69,7 @@ function Game() {
           <Card
             key={card.id}
             card={card}
-            selected={card.id === selectedCardId}
+            selected={foundCards.includes(card.id)}
             handleClick={() => handleCardClick(card.id)}
           />
         ))}
